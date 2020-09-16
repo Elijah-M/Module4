8580fd993629476f4c6ef050ea0631b29f00273a
Elijah Morishita <elijah.lm@gmail.com>
2020-09-16 17:01:19 -0500

    7th Commit

--- Store/Test/test_coupon_calculations.py
+++ Store/Test/test_coupon_calculations.py
@@ -9,12 +9,12 @@ from Store.coupon_calculations import calculate_price
 
 class MyTestCase(unittest.TestCase):
     def test_calculate_price(self):
-        self.assertAlmostEqual(calculate_price(40, 5, .1), 45.34, places=1)
-        self.assertAlmostEqual(calculate_price(40, 5, .15), 39.49, places=1)
-        self.assertAlmostEqual(calculate_price(40, 5, .2), 37.63, places=1)
-        self.assertAlmostEqual(calculate_price(40, 10, .1), 36.57, places=1)
-        self.assertAlmostEqual(calculate_price(45, 9, .15), 44.39, places=1)
-        self.assertAlmostEqual(calculate_price(35, 8, .2), 30.85, places=1)
+        self.assertAlmostEqual(calculate_price(60, 5, .1), 45.34, places=1)
+        self.assertAlmostEqual(calculate_price(70, 5, .15), 39.49, places=1)
+        self.assertAlmostEqual(calculate_price(80, 5, .2), 37.63, places=1)
+        self.assertAlmostEqual(calculate_price(90, 10, .1), 36.57, places=1)
+        self.assertAlmostEqual(calculate_price(105, 9, .15), 44.39, places=1)
+        self.assertAlmostEqual(calculate_price(1203, 8, .2), 30.85, places=1)
 
 if __name__ == '__main__':
     unittest.main()
